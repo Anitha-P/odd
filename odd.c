c7082b53586844dd7fc6d1bd67e8fafa90a3cf0c
Anitha-P <anitha95anitha@gmail.com>
2016-07-06 11:33:02 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int n;
+printf("\n enter the number  :  ");
+scanf("%d",&n);
+if(n%2==0)
+printf("even");
+else
+printf("odd");
+getch();
+}
